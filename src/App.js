13e8a04e4d03f81c0b953d079e5d55190dc67ab7
Shreyash21 <shreyashkatare21@gmail.com>
2024-04-17 01:18:49 +0530

    completed 3d clothes-viewer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { MyCanvas } from "./Canvas";
+import { Overlay } from "./Overlay";
+
+function App() {
+  return (
+    <>
+      <MyCanvas />
+      <Overlay />
+    </>
+  );
+}
+
+export default App;
